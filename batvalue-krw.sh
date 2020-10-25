10f68d15e631ff3ef7ad6e1d15cb40d7e8765f4b
Minho <gastonglpi@outlook.com>
2020-10-25 04:14:08 +0100

    Add JPY/KRW

---
+++ batvalue-krw.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# This bash script is provide by
+#	- Twitter: @Minho_IT
+#	- GitHub: minho-it
+#
+# Basically, it just get bat data from uphold data.
+
+
+# Get BAT > KRW value from uphold api (no api key require)
+URL=$(curl -s https://api.uphold.com/v0/ticker/BATKRW | jq -r .bid)
+
+# Managing color
+LP='\033[1;45m'
+ORANGE='\033[0;33m'
+PURPLE='\033[0;35m'
+RED='\033[0;31m'
+CYAN='\033[1;46m'
+NC='\033[0m'
+
+function showbat() {
+
+	echo " "
+        echo "———————————————————————————————————————————"
+	printf "${ORANGE}Bat${PURPLE}Val${RED}ue:${NC}"
+	echo " "
+	echo " "
+	printf "${CYAN} 1 BAT ${NC} 동일 ${LP} ${URL} 원 ${NC}"
+	echo " "
+	echo " "
+	echo "———————————————————————————————————————————"
+	echo " "
+}
